010891f2a45031976f7184a9ab3f9bbb12787714
Harshit Harsh <104749996+hharsh05@users.noreply.github.com>
2022-07-14 20:06:52 +0530

    Update myapplication.py

--- myapplication.py
+++ myapplication.py
@@ -12,6 +12,7 @@ st.text("Created by Mr.Harshit Harsh")
 # Input bar 1
 HeartDiseaseorAttack = st.number_input("Enter 1 if you suffer from Heart Disease or had an attack else enter 0")
 
+
 # Input bar 2
 BMI = st.number_input("Enter BMI, BMI = Weight(Kg)/(Height*Height(metre squared))")
 HvyAlcoholConsump = st.number_input("Enter 1 if you consume alcohol frequently else enter 0")
@@ -20,19 +21,21 @@ Smoker = st.number_input("Enter 1 if you are smoker  else enter 0")
 
 # If button is pressed
 if st.button("Submit"):
-
-    # Unpickle classifier
-    clf = joblib.load("clf.pkl")
+    if ((HeartDiseaseorAttack)!=0 or 1) and (HvyAlcoholConsump!=0 or 1) and (Smoker!=0 or 1):
+        st.text("Please follow instruction")
+    else:
+        clf = joblib.load("clf.pkl")
 
     # Store inputs into dataframe
-    x = pd.DataFrame([[HeartDiseaseorAttack,BMI,Smoker,HvyAlcoholConsump]],
+        x = pd.DataFrame([[HeartDiseaseorAttack,BMI,Smoker,HvyAlcoholConsump]],
                      columns = ["HeartDiseaseorAttack","BMI","Smoker","HvyAlcoholConsump"])
     # Get prediction
-    prediction = clf.predict(x)[0]
+        prediction = clf.predict(x)[0]
 
     # Output prediction
-    st.text(f"This instance is a {prediction}")
-    if (prediction == 1 or 2):
-        st.text("Consult a Doctor")
+        st.text(f"This instance is a {prediction}")
+        if (prediction == 1 or 2):
+            st.text("Consult a Doctor")
     
-  
+        
+   
